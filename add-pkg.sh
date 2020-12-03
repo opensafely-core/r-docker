7f524dcd7ed6bb952783fb8a6820974b7c4ef269
bloodearnest <bloodearnest@gmail.com>
2020-12-03 15:38:49 +0000

    Add odbc and sccs packages
    Also some slight tweaks to the pkg scripts to use ghcr.io/opensafely/r image

--- add-pkg.sh
+++ add-pkg.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -eux
 PACKAGE=$1
-IMAGE=${2:-docker.opensafely.org/r}
+IMAGE=${2:-ghcr.io/opensafely/r}
+# docker tags need to be lowercase
+NAME=$(echo $PACKAGE | tr A-Z a-z)
 
 docker tag $IMAGE r-backup
 # build
-docker run --name $PACKAGE docker.opensafely.org/r -e "install.packages('$PACKAGE')"
-docker commit --change "CMD []" $PACKAGE r-$PACKAGE
-docker rm $PACKAGE
-docker run r-$PACKAGE -e "library('$PACKAGE')"
-./test.sh r-$PACKAGE
-docker tag r-$PACKAGE $IMAGE
+docker run --name $NAME $IMAGE -e "install.packages('$PACKAGE')"
+docker commit --change "CMD []" $NAME r-$NAME
+docker rm $NAME
+docker run r-$NAME -e "library('$PACKAGE')"
+./test.sh r-$NAME
+docker tag r-$NAME $IMAGE
 
 set +x
 echo "Run this to push:"
