cfad8dd5ff8c923467e44cff1d2ef2202a722b9a
bloodearnest <bloodearnest@gmail.com>
2021-03-02 15:23:17 +0000

    Add gtsummary
    Involved supporting adding system packages too.

--- add-pkg.sh
+++ add-pkg.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 set -eux
 PACKAGE=$1
-IMAGE=${2:-ghcr.io/opensafely-core/r}
+TYPE=${2:-r}
+IMAGE=${IMAGE:-ghcr.io/opensafely-core/r}
 # docker tags need to be lowercase
-NAME=$(echo $PACKAGE | tr A-Z a-z)
+NAME=$(echo "$PACKAGE" | tr '[:upper:]' '[:lower:]')
 
-docker tag $IMAGE r-backup
+trap 'docker container rm $NAME || true' EXIT
+
+docker tag "$IMAGE" r-backup
 # build
-docker run --name $NAME $IMAGE -e "install.packages('$PACKAGE')"
-docker commit --change "CMD []" $NAME r-$NAME
-docker rm $NAME
-docker run r-$NAME -e "library('$PACKAGE')"
-./test.sh r-$NAME
-docker tag r-$NAME $IMAGE
-echo $PACKAGE >> packages.txt
-docker run -v $PWD:/out $IMAGE -e 'write.csv(installed.packages()[, c("Package","Version")], row.names=FALSE, file="/out/packages.csv")'
+if test "$TYPE" == "r"; then
+    docker run --name "$NAME" "$IMAGE" -e "install.packages('$PACKAGE')"
+else
+    docker run --name "$NAME" --entrypoint bash "$IMAGE" -c "apt-get install -y $PACKAGE"
+fi
+docker commit --change "CMD []" --change 'ENTRYPOINT ["/usr/bin/Rscript"]' "$NAME" "r-$NAME"
+
+if test "$TYPE" == "r"; then
+    docker run "r-$NAME" -e "library('$PACKAGE')"
+    ./test.sh "r-$NAME"
+    echo "$PACKAGE" >> packages.txt
+fi
+
+docker tag "r-$NAME" "$IMAGE"
+docker run -v "$PWD:/out" "$IMAGE" -e 'write.csv(installed.packages()[, c("Package","Version")], row.names=FALSE, file="/out/packages.csv")'
 
 set +x
 echo "Run this to push:"
