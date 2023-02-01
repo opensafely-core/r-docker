4d280b6ab56778faced64070a939342ea922c143
bloodearnest <simon.davy@thedatalab.org>
2023-02-01 12:46:30 +0000

    fix tests.sh to generate better R code
    Also silence the IMAGE_TAG warning from docker-compose

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 IMAGE=${1:-ghcr.io/opensafely-core/r}
-python3 -c 'import json; print("\n".join(json.load(open("renv.lock"))["Packages"]))' | xargs -I {} echo -e "library(\"{}\", warn.conflicts = FALSE);\n" > .tests.R
+python3 -c 'import json; print("\n".join(json.load(open("renv.lock"))["Packages"]))' | xargs -I {} echo "library({}, warn.conflicts = FALSE)" > .tests.R
 docker run --rm -v "$PWD:/workspace" "$IMAGE" .tests.R
