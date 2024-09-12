de4cb06c71e7f709d89015b3aed2a6c36eecefba
Tom Palmer <remlapmot@hotmail.com>
2024-09-12 15:04:03 +0100

    Update rstudio-entrypoint.sh

--- rstudio-entrypoint.sh
+++ rstudio-entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+# Check .gitignore for some entries, add them if not present
+for item in .local .config .Rhistory .Rproj.user .Rproj.user/ .DS_Store ;
+do
+  if ! grep -q $item /home/rstudio/.gitignore; then
+    echo $item >> /home/rstudio/.gitignore
+  fi
+done
+
+# Start RStudio Server session
 rstudio-server start
+# Ensure that the docker container does not exit
 sleep infinity
