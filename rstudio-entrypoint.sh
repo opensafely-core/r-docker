73437f4b191b7ba9e34aa0c5d9a76e6623b25121
Tom Palmer <remlapmot@hotmail.com>
2024-09-13 17:46:55 +0100

    Simplify to one line

--- rstudio-entrypoint.sh
+++ rstudio-entrypoint.sh
@@ -12,8 +12,7 @@ done
 if [ $(find /home/rstudio -type f -name "*.Rproj" | wc -w) -eq 1 ] ; then
   # Avoid Git error fatal detected dubious ownership of repository if using Git in container
   # Without this the Git pane fails to open when RStudio project opened
-  echo "[safe]" >> /home/rstudio/.gitconfig
-  echo "	directory = \"*\"" >> /home/rstudio/.gitconfig
+  echo "[safe]\n	directory = \"*\"" >> /home/rstudio/.gitconfig
 
   # Rstudio hook to open project on startup if 1 .Rproj file exists
   echo "if (length(list.files(pattern = \"Rproj\")) == 1L) setHook(\"rstudio.sessionInit\", function(newSession) { if (newSession && is.null(rstudioapi::getActiveProject())) rstudioapi::openProject(list.files(pattern = \"Rproj\")) }, action = \"append\")" >> /home/rstudio/.Rprofile
