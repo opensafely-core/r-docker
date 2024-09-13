73a90eaddc7cb9d1057493d87ca971085d1ae3ff
Tom Palmer <remlapmot@hotmail.com>
2024-09-13 17:52:00 +0100

    Update rstudio-entrypoint.sh

--- rstudio-entrypoint.sh
+++ rstudio-entrypoint.sh
@@ -9,7 +9,7 @@ do
 done
 
 # Check for 1 .Rproj file
-if [ $(find /home/rstudio -type f -name "*.Rproj" | wc -w) -eq 1 ] ; then
+if [ $(find /home/rstudio -type f -name "*.Rproj" | wc -w) -eq 1 ]; then
   # Avoid Git error fatal detected dubious ownership of repository if using Git in container
   # Without this the Git pane fails to open when RStudio project opened
   echo "[safe]\n	directory = \"*\"" >> /home/rstudio/.gitconfig
