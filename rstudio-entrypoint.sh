5c8c8f8aa4abb694e98c313bc21232b81a0eafce
Tom Palmer <remlapmot@hotmail.com>
2024-09-13 17:41:21 +0100

    Simplify items added to .gitignore

--- rstudio-entrypoint.sh
+++ rstudio-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check .gitignore for some entries, add them if not present
-for item in .local .config .Rhistory .Rproj.user .Rproj.user/ .DS_Store ;
+for item in .local .config ;
 do
   if ! grep -q $item /home/rstudio/.gitignore; then
     echo $item >> /home/rstudio/.gitignore
