33db0d4491b260f50d625442f852581b030d7b30
Tom Palmer <remlapmot@hotmail.com>
2024-09-18 21:05:45 +0100

    Use /workspace as dir

--- rstudio-entrypoint.sh
+++ rstudio-entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Check .gitignore for some entries, add them if not present
-for item in .local .config;
-do
-  if ! grep -q $item /workspace/.gitignore; then
-    echo $item >> /workspace/.gitignore
-  fi
-done
+# for item in .local .config;
+# do
+#   if ! grep -q $item /workspace/.gitignore; then
+#     echo $item >> /workspace/.gitignore
+#   fi
+# done
 
 # Check for 1 .Rproj file
 if [ $(find /workspace -type f -name "*.Rproj" | wc -w) -eq 1 ]; then
   # Avoid creating setting in .gitconfig if already specified
-  if ! grep -e "\[safe\]" -e "\tdirectory = \"\*\"" /workspace/.gitconfig; then 
+  if ! grep -e "\[safe\]" -e "\tdirectory = \"\*\"" /home/rstudio/.gitconfig; then 
     # Avoid Git error fatal detected dubious ownership of repository if using Git in container
     # Without this the Git pane fails to open when RStudio project opened
-    echo -e "[safe]\n\tdirectory = \"*\"" >> /workspace/.gitconfig
+    echo -e "[safe]\n\tdirectory = \"*\"" >> /home/rstudio/.gitconfig
   fi
 fi
 
