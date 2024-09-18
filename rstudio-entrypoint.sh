d1b59262601270fd63924aaedb8f153ca4c29b9b
Tom Palmer <remlapmot@hotmail.com>
2024-09-18 21:31:08 +0100

    No need to check .gitconfig as it won't exist yet

--- rstudio-entrypoint.sh
+++ rstudio-entrypoint.sh
@@ -2,12 +2,9 @@
 
 # Check for 1 .Rproj file
 if [ $(find /workspace -type f -name "*.Rproj" | wc -w) -eq 1 ]; then
-  # Avoid creating setting in .gitconfig if already specified
-  if ! grep -e "\[safe\]" -e "\tdirectory = \"\*\"" /home/rstudio/.gitconfig; then 
-    # Avoid Git error fatal detected dubious ownership of repository if using Git in container
-    # Without this the Git pane fails to open when RStudio project opened
-    echo -e "[safe]\n\tdirectory = \"*\"" >> /home/rstudio/.gitconfig
-  fi
+  # Avoid Git error fatal detected dubious ownership of repository if using Git in container
+  # Without this the Git pane fails to open when RStudio project opened
+  echo -e "[safe]\n\tdirectory = \"*\"" >> /home/rstudio/.gitconfig
 
   # Open RStudio project on opening RStudio Server session
   echo 'setHook("rstudio.sessionInit", function(newSession) { if (newSession && is.null(rstudioapi::getActiveProject())) rstudioapi::openProject(paste0("/workspace/", list.files(pattern = "Rproj"))) }, action = "append")' >> /home/rstudio/.Rprofile
