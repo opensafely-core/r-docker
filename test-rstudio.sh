eb810ba5c2285def34f124c268b84f5971c9792f
Tom Palmer <remlapmot@hotmail.com>
2024-10-03 20:32:29 +0100

    Formatting

--- test-rstudio.sh
+++ test-rstudio.sh
@@ -9,11 +9,18 @@ else
   PLATFORM="somethingelse"
 fi
 
-docker run --rm --init --label=opensafely --interactive \
-    --user=0:0 --volume=$PWD://workspace --platform=linux/amd64 -p=8787:8787 \
-    --name=test_rstudio --hostname=test_rstudio \
-    --volume=$HOME/.gitconfig:/home/rstudio/local-gitconfig \
-    --env=HOSTPLATFORM=$PLATFORM \
+docker run \
+    --rm \
+    --init \
+    --label=opensafely \
+    --interactive \
+    --user=0:0 --volume="/${PWD}://workspace" \
+    --platform=linux/amd64 \
+    -p=8787:8787 \
+    --name=test_rstudio \
+    --hostname=test_rstudio \
+    --volume="/${HOME}/.gitconfig:/home/rstudio/local-gitconfig" \
+    --env=HOSTPLATFORM=${PLATFORM} \
     --env=HOSTUID=$(id -u) \
     rstudio
 
