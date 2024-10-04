990089580902f79821a3792c57dd188bba25414a
Tom Palmer <remlapmot@hotmail.com>
2024-10-04 06:01:27 +0100

    Update test-rstudio.sh

--- test-rstudio.sh
+++ test-rstudio.sh
@@ -28,7 +28,7 @@ sleep 5
 
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null -L http://localhost:8787)
 if [[ "$status_code" -ne 200 ]] ; then
-  echo "Response not received from http://localhost:8787"
+  echo "200 response not received from http://localhost:8787"
   docker stop test_rstudio || true
   exit 1
 else
