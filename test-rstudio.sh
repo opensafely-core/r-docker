0966c396cd3a9c881035d65c6c364372e77a3ceb
Tom Palmer <remlapmot@hotmail.com>
2024-10-04 06:00:40 +0100

    Update test-rstudio.sh

--- test-rstudio.sh
+++ test-rstudio.sh
@@ -22,16 +22,17 @@ docker run \
     --volume="/${HOME}/.gitconfig:/home/rstudio/local-gitconfig" \
     --env=HOSTPLATFORM=${PLATFORM} \
     --env=HOSTUID=$(id -u) \
-    --detach \
-    rstudio
+    rstudio &
 
-sleep 10
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8787)
+sleep 5
+
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null -L http://localhost:8787)
 if [[ "$status_code" -ne 200 ]] ; then
   echo "Response not received from http://localhost:8787"
-  docker kill test_rstudio || true
+  docker stop test_rstudio || true
   exit 1
 else
-  docker kill test_rstudio
+  echo "200 response successfully received from http://localhost:8787"
+  docker stop test_rstudio > /dev/null 2>&1
   exit 0
 fi
