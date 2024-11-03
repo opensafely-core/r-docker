399f09a1a1db1eb978c808574fb7622b5999c094
Tom Palmer <remlapmot@hotmail.com>
2024-11-03 14:46:07 +0000

    Change quotes around

--- update.sh
+++ update.sh
@@ -6,12 +6,12 @@ echo "here 2 $REPOS"
 echo "here 2 \"$REPOS\""
 
 if [ "$UPDATE" = "true" ]; then
-  R -e 'install.packages(c("renv", "pak"), repos = \"$REPOS\", destdir="/cache"); \
+  R -e "install.packages(c('renv', 'pak'), repos = \"$REPOS\", destdir='/cache'); \
     options(renv.config.pak.enabled = TRUE); \
-    pkgs <- read.csv("packages.csv")$Package; \
-    pkgs <- pkgs[pkgs != "renv"]; \
-    renv::install(pkgs, repos = \"$REPOS\", destdir="/cache"); \
+    pkgs <- read.csv('packages.csv')$Package; \
+    pkgs <- pkgs[pkgs != 'renv']; \
+    renv::install(pkgs, repos = \"$REPOS\", destdir='/cache'); \
     webshot::install_phantomjs(); \
-    renv::install("sjPlot", repos = \"$REPOS\", destdir="/cache"); \
-    renv::snapshot(type=\"all\")'
+    renv::install('sjPlot', repos = \"$REPOS\", destdir='/cache'); \
+    renv::snapshot(type='all')"
 fi
