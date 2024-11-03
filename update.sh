50d43f124bee15c8c8ea0bf64907718bb828e581
Tom Palmer <remlapmot@hotmail.com>
2024-11-03 14:51:51 +0000

    Escape $

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ echo "here 2 \"$REPOS\""
 if [ "$UPDATE" = "true" ]; then
   R -e "install.packages(c('renv', 'pak'), repos = \"$REPOS\", destdir='/cache'); \
     options(renv.config.pak.enabled = TRUE); \
-    pkgs <- read.csv('packages.csv')$Package; \
+    pkgs <- read.csv('packages.csv')\$Package; \
     pkgs <- pkgs[pkgs != 'renv']; \
     renv::install(pkgs, repos = \"$REPOS\", destdir='/cache'); \
     webshot::install_phantomjs(); \
