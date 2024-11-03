da6b3d1ed9f3c7546ad044ee05c2db700b622555
Tom Palmer <remlapmot@hotmail.com>
2024-11-03 14:46:52 +0000

    Change quotes around

--- restore.sh
+++ restore.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 if [ "$UPDATE" = "false" ]; then 
-  R -e 'install.packages(c("renv", "pak"), repos = \"$REPOS\", destdir="/cache"); \
+  R -e "install.packages(c('renv', 'pak'), repos = \"$REPOS\", destdir='/cache'); \
     options(renv.config.pak.enabled = TRUE); \
     renv::init(bare = TRUE); \
-    renv::restore()'
+    renv::restore()"
 fi
