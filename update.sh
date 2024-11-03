674d00d21b13857e5b082fd4b0fa332f55240105
Tom Palmer <remlapmot@hotmail.com>
2024-11-03 14:54:54 +0000

    Formatting

--- update.sh
+++ update.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 
 if [ "$UPDATE" = "true" ]; then
-  R -e "install.packages(c('renv', 'pak'), repos = \"$REPOS\", destdir='/cache'); \
+  R -e "install.packages(c('renv', 'pak'), repos = \"$REPOS\", destdir = '/cache'); \
     options(renv.config.pak.enabled = TRUE); \
     pkgs <- read.csv('packages.csv')\$Package; \
     pkgs <- pkgs[pkgs != 'renv']; \
-    renv::install(pkgs, repos = \"$REPOS\", destdir='/cache'); \
+    renv::install(pkgs, repos = \"$REPOS\", destdir = '/cache'); \
     webshot::install_phantomjs(); \
-    renv::install('sjPlot', repos = \"$REPOS\", destdir='/cache'); \
+    renv::install('sjPlot', repos = \"$REPOS\", destdir = '/cache'); \
     renv::snapshot(type='all')"
 fi
