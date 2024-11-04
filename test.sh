b473ead62fac0cb79e9ce1c7a79966cb5547af54
Tom Palmer <remlapmot@hotmail.com>
2024-11-04 15:35:03 +0000

    Wrap in try()
    There is currently a bug in R.utils or conflicted
    
    https://github.com/r-lib/conflicted/issues/105
    
    https://github.com/HenrikBengtsson/R.utils/issues/157

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 IMAGE=${1:-ghcr.io/opensafely-core/r}
-python3 -c 'import json; print("\n".join(json.load(open("renv.lock"))["Packages"]))' | xargs -I {} echo "library({}, warn.conflicts = FALSE)" > .tests.R
 docker run --rm -v "$PWD:/tests/" "$IMAGE" /tests/.tests.R
+python3 -c 'import json; print("\n".join(json.load(open("renv.lock"))["Packages"]))' | xargs -I {} echo "try(library({}, warn.conflicts = FALSE))" > .tests.R
