5dd6f680fdd9622d736a1a57e0c66d1513ad73a6
Tom Palmer <remlapmot@hotmail.com>
2024-11-04 15:35:22 +0000

    Add --platform and fix command

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 IMAGE=${1:-ghcr.io/opensafely-core/r}
-docker run --rm -v "$PWD:/tests/" "$IMAGE" /tests/.tests.R
 python3 -c 'import json; print("\n".join(json.load(open("renv.lock"))["Packages"]))' | xargs -I {} echo "try(library({}, warn.conflicts = FALSE))" > .tests.R
+docker run --platform linux/amd64 --rm -v "$PWD:/tests/" "$IMAGE" -e "source('/tests/.tests.R', echo = TRUE)"
