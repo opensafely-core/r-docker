bf65caa985fac6f5c5f71ae2c2f27054b12ea1a4
Tom Palmer <remlapmot@hotmail.com>
2024-11-04 20:14:28 +0000

    Better fix for test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 IMAGE=${1:-ghcr.io/opensafely-core/r}
-python3 -c 'import json; print("\n".join(json.load(open("renv.lock"))["Packages"]))' | xargs -I {} echo "try(library({}, warn.conflicts = FALSE))" > .tests.R
+python3 -c 'import json; print("\n".join(json.load(open("renv.lock"))["Packages"]))' | xargs -I {} echo "library({}, warn.conflicts = FALSE); detach(package:{})" > .tests.R
 docker run --platform linux/amd64 --rm -v "$PWD:/tests/" "$IMAGE" -e "source('/tests/.tests.R', echo = TRUE)"
