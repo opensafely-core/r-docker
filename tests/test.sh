c25c9dd398744a9d5a6fc0c7b2e4356d97d851e3
Tom Palmer <remlapmot@hotmail.com>
2024-11-05 16:27:01 +0000

    Add comment

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 IMAGE=${1:-ghcr.io/opensafely-core/r}
+
+# Test all R packages can be attached then detached
 python3 -c 'import json; print("\n".join(json.load(open("renv.lock"))["Packages"]))' | xargs -I {} echo "library({}, warn.conflicts = FALSE); suppressWarnings(detach(\"package:{}\", force = TRUE))" > .tests.R
 docker run --platform linux/amd64 --rm -v "$PWD:/tests/" "$IMAGE" -e "source('/tests/.tests.R', echo = TRUE)"
