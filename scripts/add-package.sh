b09ff51644326e71e8bb44091211022f48fc33fc
Tom Palmer <remlapmot@hotmail.com>
2024-11-06 11:39:03 +0000

    Write to MAJOR_VERSION dir

--- scripts/add-package.sh
+++ scripts/add-package.sh
@@ -31,4 +31,4 @@ fi
 just test "$IMAGE"
 
 # update packages.csv for backwards compat with current docs
-docker run --platform linux/amd64 -v "/$PWD:/out" "$IMAGE" -q -e 'write.csv(installed.packages()[, c("Package","Version")], row.names=FALSE, file="/out/packages.csv")'
+docker run --platform linux/amd64 -v "/$PWD:/out" "$IMAGE" -q -e 'write.csv(installed.packages()[, c("Package","Version")], row.names=FALSE, file=paste0("/out/", Sys.getenv(\"MAJOR_VERSION\"), "packages.csv")'
