907a226c3ba74dc3808aceb0498a378af40d981c
Tom Palmer <remlapmot@hotmail.com>
2024-11-06 14:06:35 +0000

    Different restore for v1 and v2

--- scripts/restore.R
+++ scripts/restore.R
@@ -1,19 +1,25 @@
-REPOS <- Sys.getenv("REPOS")
-CRAN_DATE <- Sys.getenv("CRAN_DATE")
+if (Sys.getenv("MAJOR_VERSION") == "v1") {
+  install.packages("renv", destdir = "/cache")
+  renv::init(bare = TRUE)
+  renv::restore()
+} else if (Sys.getenv("MAJOR_VERSION") == "v2") {
+  REPOS <- Sys.getenv("REPOS")
+  CRAN_DATE <- Sys.getenv("CRAN_DATE")
 
-# Set HTTPUserAgent so that PPPM serves binary R packages for Linux
-options(HTTPUserAgent = sprintf(
-  "R/%s R (%s)", getRversion(),
-  paste(
-    getRversion(),
-    R.version["platform"],
-    R.version["arch"],
-    R.version["os"]
-  )
-))
+  # Set HTTPUserAgent so that PPPM serves binary R packages for Linux
+  options(HTTPUserAgent = sprintf(
+    "R/%s R (%s)", getRversion(),
+    paste(
+      getRversion(),
+      R.version["platform"],
+      R.version["arch"],
+      R.version["os"]
+    )
+  ))
 
-install.packages(c("renv", "pak"), repos = c(CRAN = REPOS), destdir = "/cache")
-options(renv.config.pak.enabled = TRUE)
-pak::repo_add(CRAN = paste0("RSPM@", CRAN_DATE))
-renv::init(bare = TRUE)
-renv::restore()
+  install.packages(c("renv", "pak"), repos = c(CRAN = REPOS), destdir = "/cache")
+  options(renv.config.pak.enabled = TRUE)
+  pak::repo_add(CRAN = paste0("RSPM@", CRAN_DATE))
+  renv::init(bare = TRUE)
+  renv::restore()
+}
