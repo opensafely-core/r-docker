0e2eec152c8bd393b96c72d63c38471e8f955dec
Tom Palmer <remlapmot@hotmail.com>
2024-11-07 21:43:53 +0000

    Amend docker compose command

--- tests/test.sh
+++ tests/test.sh
@@ -7,4 +7,4 @@ python3 -c "import json; print('\n'.join(json.load(open(\"./$MAJOR_VERSION/renv.
 docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r:"${MAJOR_VERSION}" -e "source('/tests/.tests.R', echo = TRUE)"
 
 # Check that a basic Rcpp call runs successfully
-docker compose run --rm r:"$MAJOR_VERSION" -e "Rcpp::evalCpp('2 + 2')"
+docker compose --env-file ${MAJOR_VERSION}/env run --rm r -e "Rcpp::evalCpp('2 + 2')"
