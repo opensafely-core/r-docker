b88b87a95ca37afbbd2bf82392bd03f612a6aa6e
Tom Palmer <remlapmot@hotmail.com>
2024-11-07 14:55:09 +0000

    Fix path

--- tests/test.sh
+++ tests/test.sh
@@ -3,7 +3,7 @@ set -eu
 MAJOR_VERSION=${1}
 
 # Test all R packages can be attached then detached
-python3 -c 'import json; print("\n".join(json.load(open("renv.lock"))["Packages"]))' | xargs -I {} echo "library({}, warn.conflicts = FALSE); suppressWarnings(detach(\"package:{}\", force = TRUE))" > /tests/.tests.R
+python3 -c 'import json; print("\n".join(json.load(open("${MAJOR_VERSION}/renv.lock"))["Packages"]))' | xargs -I {} echo "library({}, warn.conflicts = FALSE); suppressWarnings(detach(\"package:{}\", force = TRUE))" > /tests/.tests.R
 docker compose --env-file ${MAJOR_VERSION}/env run --platform linux/amd64 --rm -v "${PWD}/tests:/tests/" r:"${MAJOR_VERSION}" -e "source('/tests/.tests.R', echo = TRUE)"
 
 # Check that a basic Rcpp call runs successfully
