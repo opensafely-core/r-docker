f9ce41770c433d067729ae9920bd3b05b9fd26bd
Tom Palmer <remlapmot@hotmail.com>
2024-11-07 15:00:31 +0000

    Something broken

--- tests/test.sh
+++ tests/test.sh
@@ -3,7 +3,8 @@ set -eu
 MAJOR_VERSION=${1}
 
 # Test all R packages can be attached then detached
-python3 -c 'import json; print("\n".join(json.load(open("${MAJOR_VERSION}/renv.lock"))["Packages"]))' | xargs -I {} echo "library({}, warn.conflicts = FALSE); suppressWarnings(detach(\"package:{}\", force = TRUE))" > /tests/.tests.R
+echo "$MAJOR_VERSION"
+python3 -c "import json; print('\n'.join(json.load(open(\"${MAJOR_VERSION}/renv.lock\"))['Packages']))" | xargs -I {} echo "library({}, warn.conflicts = FALSE); suppressWarnings(detach(\"package:{}\", force = TRUE))" > /tests/.tests.R
 docker compose --env-file ${MAJOR_VERSION}/env run --platform linux/amd64 --rm -v "${PWD}/tests:/tests/" r:"${MAJOR_VERSION}" -e "source('/tests/.tests.R', echo = TRUE)"
 
 # Check that a basic Rcpp call runs successfully
