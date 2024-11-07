19b04e88e8cba47c6ff46ccf1d972f3d797243f1
Tom Palmer <remlapmot@hotmail.com>
2024-11-07 21:14:10 +0000

    Fix test script

--- tests/test.sh
+++ tests/test.sh
@@ -3,9 +3,8 @@ set -eu
 MAJOR_VERSION=${1}
 
 # Test all R packages can be attached then detached
-echo "$MAJOR_VERSION"
-python3 -c "import json; print('\n'.join(json.load(open(\"${MAJOR_VERSION}/renv.lock\"))['Packages']))" | xargs -I {} echo "library({}, warn.conflicts = FALSE); suppressWarnings(detach(\"package:{}\", force = TRUE))" > /tests/.tests.R
-docker compose --env-file ${MAJOR_VERSION}/env run --rm -v "${PWD}:/" r:"${MAJOR_VERSION}" -e "source('/tests/.tests.R', echo = TRUE)"
+python3 -c "import json; print('\n'.join(json.load(open(\"./$MAJOR_VERSION/renv.lock\"))['Packages']))" | xargs -I {} echo "library({}, warn.conflicts = FALSE); suppressWarnings(detach(\"package:{}\", force = TRUE))" > .tests.R
+docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r:"${MAJOR_VERSION}" -e "source('/tests/.tests.R', echo = TRUE)"
 
 # Check that a basic Rcpp call runs successfully
 docker compose run --rm r:"$MAJOR_VERSION" -e "Rcpp::evalCpp('2 + 2')"
