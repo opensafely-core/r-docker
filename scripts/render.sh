1e268e3a49b039468bf3f64903d44d0c44a4db38
Tom Palmer <remlapmot@hotmail.com>
2024-11-07 20:50:39 +0000

    Debugging

--- scripts/render.sh
+++ scripts/render.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "BASE=$BASE" >> /usr/lib/R/etc/Renviron.site
 echo "MAJOR_VERSION=$MAJOR_VERSION" >> /usr/lib/R/etc/Renviron.site
+R -q -e "print(Sys.getenv('MAJOR_VERSION'))"
 R -q -e "rmarkdown::render('/out/scripts/packages.Rmd', output_dir = paste0('/out/', \"$MAJOR_VERSION\"))"
