8a42bae4dc45f13f0c405d940aa16f6fb783fba4
Tom Palmer <remlapmot@hotmail.com>
2024-11-10 14:12:32 +0000

    Comment out debug stuff

--- scripts/add-package.R
+++ scripts/add-package.R
@@ -7,28 +7,28 @@ if (Sys.getenv("MAJOR_VERSION") == "v1") {
 } else if (Sys.getenv("MAJOR_VERSION") == "v2") {
   CRAN_DATE <- Sys.getenv("CRAN_DATE")
 
-  print(Sys.getenv())
-  print(.libPaths())
-  print(Sys.getenv("MAJOR_VERSION"))
-  print(CRAN_DATE)
-  print(PACKAGE)
-  print(nrow(installed.packages()))
-  print(getwd())
-  print(list.files())
-  print("/cache/binary")
-  print(list.files("/cache/binary"))
+  # print(Sys.getenv())
+  # print(.libPaths())
+  # print(Sys.getenv("MAJOR_VERSION"))
+  # print(CRAN_DATE)
+  # print(PACKAGE)
+  # print(nrow(installed.packages()))
+  # print(getwd())
+  # print(list.files())
+  # print("/cache/binary")
+  # print(list.files("/cache/binary"))
   
-  print("/cache/binary/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu/repsository")
-  print(list.files("/cache/binary/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu/repository/pak"))
+  # print("/cache/binary/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu/repsository")
+  # print(list.files("/cache/binary/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu/repository/pak"))
 
-  print("/cache/cache/v5/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu/pak/0.8.0")
-  print(list.files("/cache/cache/v5/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu/pak/0.8.0"))
+  # print("/cache/cache/v5/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu/pak/0.8.0")
+  # print(list.files("/cache/cache/v5/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu/pak/0.8.0"))
   
-  print("/renv/lib")
-  print(list.files("/renv/lib"))
+  # print("/renv/lib")
+  # print(list.files("/renv/lib"))
   
-  print("/renv/lib/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu")
-  print(list.files("/renv/lib/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu"))
+  # print("/renv/lib/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu")
+  # print(list.files("/renv/lib/linux-ubuntu-noble/R-4.4/x86_64-pc-linux-gnu"))
   
   # Set HTTPUserAgent so that PPPM serves binary R packages for Linux
   options(HTTPUserAgent = sprintf(
