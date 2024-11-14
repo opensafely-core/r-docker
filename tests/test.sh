2c8f9a0b11c3a98a76c5f2589070f2ea37a563bb
Tom Palmer <remlapmot@hotmail.com>
2024-11-14 10:55:57 +0000

    Don't test presence of pak

--- tests/test.sh
+++ tests/test.sh
@@ -3,7 +3,7 @@ set -eu
 MAJOR_VERSION=${1}
 
 # Test all R packages can be attached then detached
-python3 -c "import json; print('\n'.join(json.load(open(\"./$MAJOR_VERSION/renv.lock\"))['Packages']))" | xargs -I {} echo "suppressPackageStartupMessages(library({}, warn.conflicts = FALSE)); suppressWarnings(detach(\"package:{}\", force = TRUE, unload = TRUE))" > .tests.R
+python3 -c "import json; print('\n'.join(json.load(open(\"./$MAJOR_VERSION/renv.lock\"))['Packages']))" | xargs -I {} echo "if (\"{}\"" != 'pak') suppressPackageStartupMessages(library({}, warn.conflicts = FALSE)); suppressWarnings(detach(\"package:{}\", force = TRUE, unload = TRUE))" > .tests.R
 docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r:"${MAJOR_VERSION}" -q -e "source('/tests/.tests.R', echo = TRUE)"
 
 # Check that a basic Rcpp call runs successfully
