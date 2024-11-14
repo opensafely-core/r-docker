377048c6a72672b701173e705eabec45319fde41
Tom Palmer <remlapmot@hotmail.com>
2024-11-14 11:53:58 +0000

    Simplify add-package.R

--- scripts/add-package.R
+++ scripts/add-package.R
@@ -5,7 +5,6 @@ if (Sys.getenv("MAJOR_VERSION") == "v1") {
   renv::install(PACKAGE)
   renv::snapshot(type = "all")
 } else if (Sys.getenv("MAJOR_VERSION") == "v2") {
-  CRAN_DATE <- Sys.getenv("CRAN_DATE")
   REPOS <- Sys.getenv("REPOS")
 
   # Set HTTPUserAgent so that PPPM serves binary R packages for Linux
@@ -19,10 +18,7 @@ if (Sys.getenv("MAJOR_VERSION") == "v1") {
     )
   ))
 
-  install.packages(c("pak", "renv"), repos = REPOS)
-  options(renv.config.pak.enabled = TRUE)
-  pak::repo_add(CRAN = paste0("RSPM@", CRAN_DATE))
-  renv::activate(project = "/renv")
-  renv::install(PACKAGE)
-  renv::snapshot(type = "all", force = TRUE)
+  renv::activate()
+  renv::install(PACKAGE, repos = REPOS)
+  renv::snapshot(type = "all")
 }
