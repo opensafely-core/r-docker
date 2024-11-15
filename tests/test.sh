65657a8535041b200a1d660ec659ab98a234a8fd
Tom Palmer <remlapmot@hotmail.com>
2024-11-15 14:10:29 +0000

    Remove version tags

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,7 @@ MAJOR_VERSION=${1}
 
 # Test all R packages can be attached then detached
 python3 -c "import json; print('\n'.join(json.load(open(\"./$MAJOR_VERSION/renv.lock\"))['Packages']))" | xargs -I {} echo "if (\"{}\" != 'pak') {suppressPackageStartupMessages(library({}, warn.conflicts = FALSE)); suppressWarnings(detach(\"package:{}\", force = TRUE, unload = TRUE))}" > .tests.R
-docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r:"${MAJOR_VERSION}" -q -e "source('/tests/.tests.R', echo = TRUE)"
+docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r -q -e "source('/tests/.tests.R', echo = TRUE)"
 
 # Check that a basic Rcpp call runs successfully
 docker compose --env-file ${MAJOR_VERSION}/env run --rm r -q -e "Rcpp::evalCpp('2 + 2')"
