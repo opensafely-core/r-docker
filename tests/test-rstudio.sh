209d6b34950d491f65313683785bb47adb063a0d
Tom Palmer <remlapmot@hotmail.com>
2024-11-16 07:08:26 +0000

    Increase retry and max-time

--- tests/test-rstudio.sh
+++ tests/test-rstudio.sh
@@ -7,7 +7,7 @@ trap "docker compose kill > /dev/null 2>&1 || true" EXIT
 
 docker compose --env-file ${MAJOR_VERSION}/env up -d --wait --wait-timeout 30 rstudio
 
-status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null -L --retry 3 --max-time 10 http://localhost:8787)
+status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null -L --retry 10 --max-time 30 http://localhost:8787)
 if [[ "$status_code" -ne 200 ]] ; then
   echo "200 response not received from http://localhost:8787"
   exit 1
