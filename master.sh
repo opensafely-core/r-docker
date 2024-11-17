398faf1b244cde708e59be077033fdac21fca3a4
Tom Palmer <remlapmot@hotmail.com>
2024-11-17 11:36:03 +0000

    Update master.sh

--- master.sh
+++ master.sh
@@ -4,6 +4,11 @@ docker build --platform linux/amd64 -t remlapmot/r-docker:r-v1-for-will .
 # docker push remlapmot/r-docker:r-v1-for-will
 # docker tag remlapmot/r-docker:r-v1-for-will ghcr.io/opensafely-core/r:latest
  
-docker build --platform linux/amd64 -t remlapmot/r-docker:rstudio-v1-for-will -f rstudio.Dockerfile .
+# docker build --platform linux/amd64 -t remlapmot/r-docker:rstudio-v1-for-will -f rstudio.Dockerfile .
+# docker push remlapmot/r-docker:rstudio-v1-for-will
+# docker tag remlapmot/r-docker:rstudio-v1-for-will ghcr.io/opensafely-core/rstudio:latest
+
+docker pull ghcr.io/opensafely-core/rstudio:latest
+docker build --platform linux/amd64 -t remlapmot/r-docker:rstudio-v1-for-will -f rstudio-2.Dockerfile .
 # docker push remlapmot/r-docker:rstudio-v1-for-will
 # docker tag remlapmot/r-docker:rstudio-v1-for-will ghcr.io/opensafely-core/rstudio:latest
