a17dabbc2849786001f8dac35f11f0e745564adf
Tom Palmer <remlapmot@hotmail.com>
2024-11-17 14:21:47 +0000

    Add instructions for Will

--- master.sh
+++ master.sh
@@ -11,3 +11,10 @@ docker build --pull --platform linux/amd64 -t remlapmot/r-docker:r-v1-for-will .
 docker build --pull --platform linux/amd64 -t remlapmot/r-docker:rstudio-v1-for-will -f rstudio-2.Dockerfile .
 # docker push remlapmot/r-docker:rstudio-v1-for-will
 # docker tag remlapmot/r-docker:rstudio-v1-for-will ghcr.io/opensafely-core/rstudio:latest
+
+# Instructions for Will
+# docker pull --platform linux/amd64 remlapmot/r-docker:r-v1-for-will
+# docker tag remlapmot/r-docker:r-v1-for-will ghcr.io/opensafely-core/r:latest
+# docker pull --platform linux/amd64 remlapmot/r-docker:rstudio-v1-for-will
+# docker tag remlapmot/r-docker:rstudio-v1-for-will ghcr.io/opensafely-core/rstudio:latest
+# opensafely rstudio
