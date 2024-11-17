5c684f1d0986e8e66760793512556a70a3412762
Tom Palmer <remlapmot@hotmail.com>
2024-11-17 14:21:30 +0000

    Add --pull

--- master.sh
+++ master.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker build --platform linux/amd64 -t remlapmot/r-docker:r-v1-for-will .
+docker build --pull --platform linux/amd64 -t remlapmot/r-docker:r-v1-for-will .
 # docker run --rm -it --platform linux/amd64 remlapmot/r-docker:r-v1-for-will
 # docker push remlapmot/r-docker:r-v1-for-will
 # docker tag remlapmot/r-docker:r-v1-for-will ghcr.io/opensafely-core/r:latest
@@ -8,7 +8,6 @@ docker build --platform linux/amd64 -t remlapmot/r-docker:r-v1-for-will .
 # docker push remlapmot/r-docker:rstudio-v1-for-will
 # docker tag remlapmot/r-docker:rstudio-v1-for-will ghcr.io/opensafely-core/rstudio:latest
 
-docker pull ghcr.io/opensafely-core/rstudio:latest
-docker build --platform linux/amd64 -t remlapmot/r-docker:rstudio-v1-for-will -f rstudio-2.Dockerfile .
+docker build --pull --platform linux/amd64 -t remlapmot/r-docker:rstudio-v1-for-will -f rstudio-2.Dockerfile .
 # docker push remlapmot/r-docker:rstudio-v1-for-will
 # docker tag remlapmot/r-docker:rstudio-v1-for-will ghcr.io/opensafely-core/rstudio:latest
