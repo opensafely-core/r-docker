85747fa8233509914d893069faa33538d2d947d6
Tom Palmer <remlapmot@hotmail.com>
2024-11-19 08:23:39 +0000

    Fixes for Rscript

--- tests/test.sh
+++ tests/test.sh
@@ -4,10 +4,10 @@ MAJOR_VERSION=${1}
 
 # Test all R packages can be attached then detached
 python3 -c "import json; print('\n'.join(json.load(open(\"./$MAJOR_VERSION/renv.lock\"))['Packages']))" | xargs -I {} echo "if (\"{}\" != 'pak') {suppressPackageStartupMessages(library({}, warn.conflicts = FALSE)); suppressWarnings(detach(\"package:{}\", force = TRUE, unload = TRUE))}" > .tests.R
-docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r -q -e "source('/tests/.tests.R', echo = TRUE)"
+docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r -e "source('/tests/.tests.R', echo = TRUE)"
 
 # Check that a basic Rcpp call runs successfully
-docker compose --env-file ${MAJOR_VERSION}/env run --rm r -q -e "Rcpp::evalCpp('2 + 2')"
+docker compose --env-file ${MAJOR_VERSION}/env run --rm r -e "Rcpp::evalCpp('2 + 2')"
 
 # Check number of packages
-docker compose --env-file ${MAJOR_VERSION}/env run --rm r -q -e "nrow(installed.packages())"
+docker compose --env-file ${MAJOR_VERSION}/env run --rm r -e "nrow(installed.packages())"
