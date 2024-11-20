993f64f92927d2616078423f01dcfbab13f90d52
Tom Palmer <remlapmot@hotmail.com>
2024-11-20 14:18:05 +0000

    Additionally check date in renv.lock

--- scripts/update-restore-add.sh
+++ scripts/update-restore-add.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-# If stub of PPPM URL in renv.lock do a restore (and possibly add a package)
-if grep -q https://packagemanager.posit.co/cran/ /renv/renv.lock; then
+
+# Extract the date from the renv.lock file
+RENVCRANDATE=$(grep https://packagemanager.posit.co/cran/ /renv/renv.lock | grep -Eo '[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}')
+
+# If stub of PPPM URL in renv.lock and CRAN_DATE matches that in renv.lock do a restore
+# (and possibly add a package)
+if grep -q https://packagemanager.posit.co/cran/ /renv/renv.lock && [ "$RENVCRANDATE" = "${CRAN_DATE}" ]; then
   Rscript /root/restore.R
 else
 # If stub of PPPM URL not in renv.lock do an update
