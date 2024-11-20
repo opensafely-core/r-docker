bc62b40be38ca884b6a96296ae27afebd3f159d0
Tom Palmer <remlapmot@hotmail.com>
2024-11-20 09:52:34 +0000

    Fix typo and method of running script

--- tests/test.sh
+++ tests/test.sh
@@ -7,8 +7,8 @@ if [ "${MAJOR_VERSION}" = "v1" ]; then
   docker run --platform linux/amd64 --rm -v "$PWD:/tests/" r:${MAJOR_VERSION} /tests/.tests.R
 elif [ "${MAJOR_VERSION}" = "v2" ]; then
   # Test all R packages can be attached then detached
-  python3 -c "import json; print('\n'.join(json.load(open(\"./$MAJOR_VERSION/renv.lock\"))['Packages']))" | xargs -I {} echo "suppressPackageStartupMessages(library({}, warn.conflicts = FALSE)); suppressWarning(detach(\"package:{}\", force = TRUE, unload = TRUE))" > .tests.R
-  docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r:"${MAJOR_VERSION}" -e "source('/tests/.tests.R', echo = TRUE)"
+  python3 -c "import json; print('\n'.join(json.load(open(\"./$MAJOR_VERSION/renv.lock\"))['Packages']))" | xargs -I {} echo "suppressPackageStartupMessages(library({}, warn.conflicts = FALSE)); suppressWarnings(detach(\"package:{}\", force = TRUE, unload = TRUE))" > .tests.R
+  docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r:"${MAJOR_VERSION}" /tests/.tests.R
 fi
 
 # Check that a basic Rcpp call runs successfully
