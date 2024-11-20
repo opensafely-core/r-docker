6566d66265c3150422c880cc92e432881da40fa5
Tom Palmer <remlapmot@hotmail.com>
2024-11-20 09:09:39 +0000

    Create restore.R

---
+++ scripts/restore.R
@@ -0,0 +1,30 @@
+REPOS <- Sys.getenv("REPOS")
+CRAN_DATE <- Sys.getenv("CRAN_DATE")
+PACKAGE <- Sys.getenv("PACKAGE")
+
+# Set HTTPUserAgent so that PPPM serves binary R packages for Linux
+options(HTTPUserAgent = sprintf(
+  "R/%s R (%s)", getRversion(),
+  paste(
+    getRversion(),
+    R.version["platform"],
+    R.version["arch"],
+    R.version["os"]
+  )
+))
+
+install.packages("renv", repos = c(CRAN = REPOS))
+renv::install("pak", repos = c(CRAN = REPOS))
+options(renv.config.pak.enabled = TRUE)
+pak::repo_add(CRAN = paste0("RSPM@", CRAN_DATE))
+renv::init(bare = TRUE)
+renv::restore()
+# To obtain pak in the final set of installed packages we seem to need to reinstall pak
+renv::install("pak")
+
+# Install additional package
+if (!PACKAGE %in% c("", "pak")) {
+  renv::install(PACKAGE)
+}
+
+renv::snapshot(type = "all")
