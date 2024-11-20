d19b48caf50c33e5753579a71ac14952bd453c41
Tom Palmer <remlapmot@hotmail.com>
2024-11-20 10:52:14 +0000

    Add -q flag

--- scripts/render.sh
+++ scripts/render.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 echo "BASE=$BASE" >> /usr/lib/R/etc/Renviron.site
 echo "MAJOR_VERSION=$MAJOR_VERSION" >> /usr/lib/R/etc/Renviron.site
-R -e "rmarkdown::render('/out/scripts/packages.Rmd', output_dir = paste0('/out/', \"$MAJOR_VERSION\"))"
+R -q -e "rmarkdown::render('/out/scripts/packages.Rmd', output_dir = paste0('/out/', \"$MAJOR_VERSION\"))"
