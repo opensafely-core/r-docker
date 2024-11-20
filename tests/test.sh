19e52324075c83f94ce45941ecf8179475cc966d
Tom Palmer <remlapmot@hotmail.com>
2024-11-20 09:14:42 +0000

    Updates for versioning and 2 additional tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -eu
-IMAGE=${1:-ghcr.io/opensafely-core/r}
-python3 -c 'import json; print("\n".join(json.load(open("renv.lock"))["Packages"]))' | xargs -I {} echo "library({}, warn.conflicts = FALSE)" > .tests.R
-docker run --rm -v "$PWD:/tests/" "$IMAGE" /tests/.tests.R
+MAJOR_VERSION=${1}
+
+# Test all R packages can be attached then detached
+python3 -c "import json; print('\n'.join(json.load(open(\"./$MAJOR_VERSION/renv.lock\"))['Packages']))" | xargs -I {} echo "suppressPackageStartupMessages(library({}, warn.conflicts = FALSE)); suppressWarnings(detach(\"package:{}\", force = TRUE, unload = TRUE))" > .tests.R
+docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r:"${MAJOR_VERSION}" -e "source('/tests/.tests.R', echo = TRUE)"
+
+# Check that a basic Rcpp call runs successfully
+docker compose --env-file ${MAJOR_VERSION}/env run --rm rimage -e "Rcpp::evalCpp('2 + 2')"
+
+# Check number of packages
+docker compose --env-file ${MAJOR_VERSION}/env run --rm rimage -e "nrow(installed.packages())"
