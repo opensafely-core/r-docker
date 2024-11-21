35125e4ab90dd5aaa4aa4e89fefbcfa722b42ea0
Tom Palmer <remlapmot@hotmail.com>
2024-11-21 05:52:08 +0000

    Improve ordering

--- scripts/restore.R
+++ scripts/restore.R
@@ -14,10 +14,10 @@ options(HTTPUserAgent = sprintf(
 ))
 
 install.packages("renv", repos = c(CRAN = REPOS), destdir = "/cache")
+renv::init(bare = TRUE)
 renv::install("pak", repos = c(CRAN = REPOS))
 options(renv.config.pak.enabled = TRUE)
 pak::repo_add(CRAN = paste0("RSPM@", CRAN_DATE))
-renv::init(bare = TRUE)
 renv::restore()
 # To obtain pak in the final set of installed packages we seem to need to reinstall pak
 renv::install("pak")
