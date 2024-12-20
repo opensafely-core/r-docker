ec34dd7a1b7278e9e241341c0cd0c15cc056e050
Tom Palmer <remlapmot@hotmail.com>
2024-12-20 15:41:53 +0000

    Use seq_along()

--- tests/test-loading-base.R
+++ tests/test-loading-base.R
@@ -1,7 +1,7 @@
 options(warn = -1)
 basepkgs <- c("base", "compiler", "datasets", "grDevices", "graphics", "grid", "methods", "parallel", "splines", "stats", "stats4", "tcltk", "tools", "utils")
 
-for (i in 1:length(basepkgs)) {
+for (i in seq_along(basepkgs)) {
   suppressMessages({
     if (!library(basepkgs[i], logical.return = TRUE, character.only = TRUE)) {
       stop(paste("Package", basepkgs[i], "failed to load, please investigate"))
