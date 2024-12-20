da87f9d5048644b532c87a88a7dc01dacf5a9921
Tom Palmer <remlapmot@hotmail.com>
2024-12-20 11:02:33 +0000

    Create build-toml.R

---
+++ scripts/build-toml.R
@@ -0,0 +1,36 @@
+REPOS <- Sys.getenv("REPOS")
+CRAN_DATE <- Sys.getenv("CRAN_DATE")
+
+# Set HTTPUserAgent so that PPPM serves binary R packages for Linux
+options(HTTPUserAgent = sprintf(
+  "R/%s R (%s)", getRversion(),
+  paste(
+    getRversion(),
+    R.version["platform"],
+    R.version["arch"],
+    R.version["os"]
+  )
+))
+
+install.packages(c("configr", "pak"), repos = c(CRAN = REPOS), destdir = "/cache")
+pak::repo_add(CRAN = paste0("RSPM@", CRAN_DATE))
+options(pkg.sysreqs = FALSE)
+
+# Read in packages.toml
+input <- configr::read.config("/tmp/packages.toml")
+
+# Obtain package names
+pkgs <- unique(na.omit(unlist(sapply(input, "[", "packages"))))
+
+# Obtain non-CRAN CRAN-like repositories, and add them to pak
+repos <- unique(na.omit(unlist(sapply(input, "[", "repos"))))
+repos <- repos[repos != ""]
+nrepos <- length(repos)
+names(repos) <- LETTERS[1:nrepos]
+pak::repo_add(.list = repos)
+
+# Create pkg.lock file
+pak::lockfile_create(pkgs, lockfile = "/pkg.lock")
+
+# Install the packages based upon the lockfile
+pak::lockfile_install("/pkg.lock")
