3757a5d3792c3bcc420d3628f92827297682b792
Tom Palmer <remlapmot@hotmail.com>
2025-03-05 20:30:24 +0000

    Refactor tests using bash functions

--- tests/test.sh
+++ tests/test.sh
@@ -2,35 +2,49 @@
 set -eu
 MAJOR_VERSION=${1}
 
+run_test_script() {
+  MAJOR_VERSION=$1
+  DIRECTORY=$2
+  TEST_SCRIPT=$3
+  docker run --platform linux/amd64 --env-file "${MAJOR_VERSION}"/env --rm -v "${PWD}:${DIRECTORY}" r:"${MAJOR_VERSION}" "${TEST_SCRIPT}"
+}
+
+run_test_command() {
+  MAJOR_VERSION=$1
+  DIRECTORY=$2
+  TEST_COMMAND=$3
+  docker run --platform linux/amd64 --env-file "${MAJOR_VERSION}"/env --rm -v "${PWD}:${DIRECTORY}" r:"${MAJOR_VERSION}" -e "${TEST_COMMAND}"
+}
+
 if [ "${MAJOR_VERSION}" = "v1" ]; then
   # Test all R packages can be loaded
   python3 -c "import json; print('\n'.join(json.load(open(\"./$MAJOR_VERSION/renv.lock\"))['Packages']))" | xargs -I {} echo "if (!library({}, warn.conflicts = FALSE, logical.return = TRUE)) {stop(\"Package {} failed to load, please investigate\")}" > .tests.R
-  docker run --platform linux/amd64 --rm -v "$PWD:/tests/" r:${MAJOR_VERSION} /tests/.tests.R
+  run_test_script "${MAJOR_VERSION}" /tests /tests/.tests.R
 elif [ "${MAJOR_VERSION}" = "v2" ]; then
   # Test all R packages can be attached then detached
-  docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r:"${MAJOR_VERSION}" /tests/tests/test-loading-packages.R
+  run_test_script "${MAJOR_VERSION}" /tests /tests/tests/test-loading-packages.R
 fi
 
 # Check that a basic Rcpp call runs successfully
-docker compose --env-file ${MAJOR_VERSION}/env run --rm r -e "if (is.numeric(Rcpp::evalCpp('2 + 2'))) {print('Rcpp test passed')}"
+run_test_command "${MAJOR_VERSION}" /tests "if (is.numeric(Rcpp::evalCpp('2 + 2'))) {print('Rcpp test passed')}"
 
 # Check number of packages
-docker compose --env-file ${MAJOR_VERSION}/env run --rm r -e "print(paste('Total number of R packages:', nrow(installed.packages())))"
+run_test_command "${MAJOR_VERSION}" /tests "print(paste('Total number of R packages:', nrow(installed.packages())))"
 
 # Check capabilities of arrow package
-docker compose --env-file ${MAJOR_VERSION}/env run --rm r -e "arrow::arrow_info()"
+run_test_command "${MAJOR_VERSION}" /tests "arrow::arrow_info()"
 
 # Test loading the 14 base packages
-docker run --platform linux/amd64 --env-file ${MAJOR_VERSION}/env --rm -v "${PWD}:/tests" r:"${MAJOR_VERSION}" /tests/tests/test-loading-base.R
+run_test_script "${MAJOR_VERSION}" /tests /tests/tests/test-loading-base.R
 
 # Test user installed paackages on v2
 if [ "${MAJOR_VERSION}" = "v2" ]; then
   # Test installing and loading in same R session
-  docker run --platform linux/amd64 --env-file "${MAJOR_VERSION}"/env --rm -v "${PWD}:/workspace" r:"${MAJOR_VERSION}" -e "unlink('/workspace/lib', recursive = TRUE)"
-  docker run --platform linux/amd64 --env-file "${MAJOR_VERSION}"/env --rm -v "${PWD}:/workspace" r:"${MAJOR_VERSION}" /workspace/tests/test-user-install-package.R
+  run_test_command "${MAJOR_VERSION}" /workspace "unlink('/workspace/lib', recursive = TRUE)"
+  run_test_script "${MAJOR_VERSION}" /workspace /workspace/tests/test-user-install-package.R
   
   # Test installing and loafding in different R sessions
-  docker run --platform linux/amd64 --env-file "${MAJOR_VERSION}"/env --rm -v "${PWD}:/workspace" r:"${MAJOR_VERSION}" -e "unlink('/workspace/lib', recursive = TRUE)"
-  docker run --platform linux/amd64 --env-file "${MAJOR_VERSION}"/env --rm -v "${PWD}:/workspace" r:"${MAJOR_VERSION}" /workspace/tests/test-preinstalled-user-package-step-1.R
-  docker run --platform linux/amd64 --env-file "${MAJOR_VERSION}"/env --rm -v "${PWD}:/workspace" r:"${MAJOR_VERSION}" /workspace/tests/test-preinstalled-user-package-step-2.R
+  run_test_command ${MAJOR_VERSION} /workspace "unlink('/workspace/lib', recursive = TRUE)"
+  run_test_script ${MAJOR_VERSION} /workspace /workspace/tests/test-preinstalled-user-package-step-1.R
+  run_test_script ${MAJOR_VERSION} /workspace /workspace/tests/test-preinstalled-user-package-step-2.R
 fi
